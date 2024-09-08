fa87df399d30b3ce7d6ec448871d953b97ab7625
root <root@22b8e4435ef0>
2024-09-08 13:33:00 +0000

    chore: created movie avro pipeline push for producer and consumer

--- schema-registry-producer/src/main/java/com/example/SchemaRegistryProducerTask.java
+++ schema-registry-producer/src/main/java/com/example/SchemaRegistryProducerTask.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.errors.SerializationException;
@@ -36,9 +37,30 @@ public class SchemaRegistryProducerTask {
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
 
-        //enter solution here
+        // To see the production of this producer we need to open a topic view of the movies topic
+        Producer<String, String> producer = new KafkaProducer<>(props);
 
+        try {
+            for (Map.Entry<String, String> movie : moviesActorMap.entrySet()) {
+                String key = movie.getKey();
+                String val = movie.getValue();
+                ProducerRecord<String, String> record = new ProducerRecord<>(SchemaRegistryProducerTask.TOPIC, key, val);
+
+                producer.send(record);
+
+                Thread.sleep(1000);
+            }   
+
+
+            producer.flush();
+            System.out.println("sent all messages to kafka topic");
+        } catch (final SerializationException e) {
+            e.printStackTrace();
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
